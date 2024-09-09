71c45004d0b2359057d50a6f43ab18055f211f82
Carsten Schlegel <71234951+bugfrei@users.noreply.github.com>
2024-09-09 12:25:16 +0200

    3 verschiedene Buttons + Controller Logik für Farbänderung

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -7,6 +7,31 @@ function (Controller) {
     return Controller.extend("project1.controller.View1", {
         onInit: function () {
 
+        },
+        setColors: function( c1, c2, c3 ) {
+            const btns = [
+                { id: "btnId1", color: c1 },
+                { id: "btnId2", color: c2 },
+                { id: "btnId3", color: c3 }
+            ];
+
+            const allColors = [ "Red", "Green", "Blue" ];
+            btns.forEach( (btn) => {
+                const oButton = this.byId(btn.id);
+                allColors.forEach( (color) => {
+                    oButton.removeStyleClass(`btn${color}`);
+                });
+                oButton.addStyleClass(`btn${btn.color}`);
+            });
+        },
+        onPressHandler1: function () {
+            this.setColors( "Green", "Blue", "Red" );
+        },
+        onPressHandler2: function () {
+            this.setColors( "Red", "Green", "Blue" );
+        },
+        onPressHandler3: function () {
+            this.setColors( "Blue", "Red", "Green" );
         }
     });
 });
